51390e2496a1c1efb69d4f913f9d1517523a94a9
Sahil Rajput <rajpootsahil51@gmail.com>
2026-02-27 00:42:38 +0530

    add check for deployment

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ const isAuth = (req, res, next) => {
   return res.redirect("/login");
 };
 
+//<------------added for deployment safety-------->
+if (!process.env.MONGODB_URI) {
+  console.error("MONGODB_URI is missing");
+  process.exit(1);
+}
+//<---------------------------------------------------->
 const sessionMiddleware = session({
   secret: process.env.SESSION_SECRET || "sahil",
   resave: false,
@@ -48,8 +54,6 @@ const server = http.createServer(app);
 const io = new Server(server);
 io.engine.use(sessionMiddleware);
 
-
-
 //<-----pages---->
 app.get("/login", (req, res) => {
   res.render("login", { user: req.session.user });
@@ -62,8 +66,6 @@ app.get("/", isAuth, (req, res) => {
   res.render("index", { user: req.session.user });
 });
 
-
-
 //<-----api---->
 app.post("/api/signup", signUpController);
 app.post("/api/login", loginController);
@@ -72,15 +74,12 @@ app.post("/api/logout", logoutController);
 //<-----checking auth--only for me----->
 app.get("/api/admin/auth/status", isAuthenticated);
 
-
-
-
 //<------websocket for leaflet------>
 io.on("connection", (socket) => {
   //console.log("A user connected with id:", socket.id);
-  const user = socket.request.session.user
+  const user = socket.request.session.user ||{};
   socket.on("send-location", (data) => {
-    io.emit("recieved-loc", { id: socket.id, ...data,...user });
+    io.emit("recieved-loc", { id: socket.id, ...data, ...user });
   });
 
   socket.on("disconnect", () => {
@@ -88,8 +87,6 @@ io.on("connection", (socket) => {
     io.emit("user-disconnected", { id: socket.id });
   });
 });
-
-
 
 app.get("/api/health-checker", (req, res) => {
   res.status(200).json({
@@ -106,6 +103,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
 
 export default app;
